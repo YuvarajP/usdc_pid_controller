a2606cf9ba6b1c5afaa3a9ada55c5d218a2e1083
Yuvaraj Patil <patil.yuvaraj@gmail.com>
2024-07-23 04:49:09 -0500

    trying new pid parameter for throttle

--- project/pid_controller/main.cpp
+++ project/pid_controller/main.cpp
@@ -251,7 +251,7 @@ int main ()
   * TODO (Step 1): create pid (pid_throttle) for throttle command and initialize values
   **/
   PID pid_throttle = PID();
-  pid_throttle.Init(0.20, 0.001, 0.5, 1, -1);
+  pid_throttle.Init(0.20, 0.001, 0.02, 1, -1);
 
   h.onMessage([&pid_steer, &pid_throttle, &new_delta_time, &timer, &prev_timer, &i, &prev_timer](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode)
   {
